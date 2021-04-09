cc3cc9f4c0cc32a2f2e6845dba2741e183bf397d
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-09 01:13:26 +0100

    Create start-up script

---
+++ run.pl
@@ -0,0 +1,8 @@
+% Serves an instance of the web app at port 8000.
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+% Load the web server.
+:- [server].
+
+% Serve it at port 8000.
+:- serve(8000).
